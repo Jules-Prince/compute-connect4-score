be0e82f7c9b2f7ad7925f6fc7e34b4cad9c61000
Theophile-Yvars <theo.yvars@gmail.com>
2023-04-13 18:56:26 +0200

    functions

--- src/Score.py
+++ src/Score.py
@@ -11,41 +11,24 @@ class Score():
         print("3 : ", num_combinations[2])
         score += num_combinations[3]*1000
         print("4 : ", num_combinations[3])   
-        return score
-    
+        return score 
+ 
     def __count_combinations(self, grid, player):
         grid = self.__convert(grid)
         print(grid)
         
         # Count the number of combinations of 1, 2, 3, and 4 aligned tokens in the grid
         num_combinations = [0, 0, 0, 0]  # 1, 2, 3, and 4 aligned tokens
+
+        self.__horizontal(grid, player, num_combinations)
+        self.__vertical(grid, player, num_combinations)
+        self.__diag1(grid, player, num_combinations)   
+        self.__diag2(grid, player, num_combinations) 
+                 
+        return num_combinations
+    
+    def __diag1(self, grid, player, num_combinations):
         count = 0
-        # Count horizontal combinations
-        for j in range(6): # 0 -> 5 # line
-            for i in range(7): # 0 -> 6 # column
-                # Count horizontal combinations
-                if grid[i][j] == player:
-                    count += 1
-                else:
-                    if count > 0:
-                        if count > 4:
-                            count = 4
-                        num_combinations[count-1] += 1
-                        count = 0
-        
-        # Count vertical combinations
-        for i in range(7): # 0 -> 6 # column
-            for j in range(6): # 0 -> 5 # line
-                # Count horizontal combinations
-                if grid[i][j] == player:
-                    count += 1
-                else:
-                    if count > 0:
-                        if count > 4:
-                            count = 4
-                        num_combinations[count-1] += 1
-                        count = 0
-                        
         #print(num_combinations)
         for i in range(13): # column
             for j in range(7): 
@@ -67,9 +50,10 @@ class Score():
                                 count = 4
                             num_combinations[count-1] += 1
                             count = 0
-        #print(num_combinations)    
-        
-        
+        #print(num_combinations)
+       
+    def __diag2(self, grid, player, num_combinations):
+        count = 0      
         #print("Diag 2")
         #print(num_combinations)
         for i in range(6, -7, -1):
@@ -89,11 +73,38 @@ class Score():
                             num_combinations[count-1] += 1
                             count = 0
                     
-        #print(num_combinations)
-        
-                        
-        return num_combinations
+        #print(num_combinations)   
     
+    def __vertical(self, grid, player, num_combinations):
+        count = 0
+        # Count vertical combinations
+        for i in range(7): # 0 -> 6 # column
+            for j in range(6): # 0 -> 5 # line
+                # Count horizontal combinations
+                if grid[i][j] == player:
+                    count += 1
+                else:
+                    if count > 0:
+                        if count > 4:
+                            count = 4
+                        num_combinations[count-1] += 1
+                        count = 0
+    
+    def __horizontal(self, grid, player, num_combinations):
+        count = 0
+        # Count horizontal combinations
+        for j in range(6): # 0 -> 5 # line
+            for i in range(7): # 0 -> 6 # column
+                # Count horizontal combinations
+                if grid[i][j] == player:
+                    count += 1
+                else:
+                    if count > 0:
+                        if count > 4:
+                            count = 4
+                        num_combinations[count-1] += 1
+                        count = 0
+      
     def __convert(self, desc):
         grid_list = []
         row = []

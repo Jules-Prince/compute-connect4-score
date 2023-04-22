b08270ffb52754f2efa500521f60bc7ed878ac73
Theophile-Yvars <theo.yvars@gmail.com>
2023-04-22 20:05:37 +0200

    fix __empty_case

--- src/MinMax.py
+++ src/MinMax.py
@@ -6,7 +6,7 @@ from src.model.Move import Move
 
 
 class MinMax:
-    def __init__(self, max_depth=5):
+    def __init__(self, max_depth=4):
         self.max_depth = max_depth
         self.score = Score()
 
@@ -17,6 +17,7 @@ class MinMax:
 
     def minimax(self, player: Piece, depth, grid: Grid):
         score = self.score.calculate_score(grid, player)
+        #print("SCORE : ", score)
 
         if depth == self.max_depth or score >= 1000:
             # return game_state.evaluate(player), None
@@ -28,8 +29,16 @@ class MinMax:
             best_value = float('inf')
         best_move = None
         stapes = self.__empty_case(grid, player)
+        
+        #print("GRID DE BASE : \n", grid)
+        
+        #print("STAPES : \n\n")
+        #for stape in stapes:
+        #    print("\n")
+        #    print(stape)
+            
         for move in stapes:
-            move = Grid("", cases=move)
+            move = Grid("", cases=move.cases)
             value, _ = self.minimax(Piece.MACHINE if player.value == Piece.HUMAN.value else Piece.HUMAN, depth + 1, move)
             if (player.value == Piece.HUMAN.value and value > best_value) or (player.value == Piece.MACHINE.value and value < best_value):
                 best_value = value
@@ -39,10 +48,14 @@ class MinMax:
 
     def __empty_case(self, grid: Grid, player: Piece) -> list:
         empty_case = []
-        for case in grid.cases:
-            if case.piece.value == Piece.BLANK.value:
+        one_empty_per_column = True
+        for index, case in enumerate(grid.cases):
+            if index % 6 == 0 and index > 0:
+                one_empty_per_column = True
+            if case.piece.value == Piece.BLANK.value and one_empty_per_column:
                 gg = copy.deepcopy(grid)
                 gg.get_case_at(case.x, case.y).piece = player
-                empty_case.append(gg.cases)
-                break
+                empty_case.append(gg)
+                #break
+                one_empty_per_column = False
         return empty_case
